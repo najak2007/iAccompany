dad5c458f84350183b0f877d3264e1a8e61114eb
najak2007 <najak2007@gmail.com>
2026-05-07 20:30:38 +0900

    i동행 앱 시작

---
+++ iAccompany/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iAccompany
+//
+//  Created by 오션블루 on 5/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iAccompany/iAccompanyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAccompanyApp.swift
+//  iAccompany
+//
+//  Created by 오션블루 on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct iAccompanyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
