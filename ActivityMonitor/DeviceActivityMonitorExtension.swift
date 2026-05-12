eb3761a422054af46231b72d002da675b6cc3ee1
najak2007 <najak2007@gmail.com>
2026-05-12 21:18:14 +0900

    모니터 추가

--- ActivityMonitor/DeviceActivityMonitorExtension.swift
+++ ActivityMonitor/DeviceActivityMonitorExtension.swift
@@ -6,43 +6,85 @@
 //
 
 import DeviceActivity
+import ManagedSettings
+import FamilyControls
+import Foundation
 
 // Optionally override any of the functions below.
 // Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
 class DeviceActivityMonitorExtension: DeviceActivityMonitor {
+    let store = ManagedSettingsStore()
+    
+    // 장치 활동 간격이 시작
     override func intervalDidStart(for activity: DeviceActivityName) {
         super.intervalDidStart(for: activity)
         
-        // Handle the start of the interval.
+        if let selection = loadSelectedApps() {
+            print("앱 목록 로드 성공")
+            blockSelectedApps(selection)
+        }
     }
     
+    // 장치 활동 간격이 끝
     override func intervalDidEnd(for activity: DeviceActivityName) {
         super.intervalDidEnd(for: activity)
-        
-        // Handle the end of the interval.
     }
     
+    // 활동이 지정한 임계값에 도달하면 호출됨
     override func eventDidReachThreshold(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
         super.eventDidReachThreshold(event, activity: activity)
-        
-        // Handle the event reaching its threshold.
     }
     
+    // 활동 시작전에 startMonitoring의 warningTime분 전에 알림 (시작 n분적, 종료 n분전)
     override func intervalWillStartWarning(for activity: DeviceActivityName) {
         super.intervalWillStartWarning(for: activity)
-        
-        // Handle the warning before the interval starts.
     }
     
     override func intervalWillEndWarning(for activity: DeviceActivityName) {
         super.intervalWillEndWarning(for: activity)
-        
-        // Handle the warning before the interval ends.
     }
     
+    // 활동이 지정한 임계값에 도달할 예정일때 호출됨
     override func eventWillReachThresholdWarning(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
         super.eventWillReachThresholdWarning(event, activity: activity)
+    }
+    
+    
+    private func loadSelectedApps() -> FamilyActivitySelection? {
+        let userDefaults = UserDefaults(suiteName: "group.co.kr.oceanbleu")
+        guard let data = userDefaults?.data(forKey: "accompanyKey")
+        else {
+            return nil
+        }
         
-        // Handle the warning before the event reaches its threshold.
+        do {
+            let model = try JSONDecoder().decode(ScreenTimeModel.self, from: data)
+            return model.selectedtoLimit
+        } catch {
+            print("앱 목록 로드 실패 : \(error)")
+            return nil
+        }
+    }
+    
+    private func blockSelectedApps(_ selection: FamilyActivitySelection) {
+        // 앱 차단 설정
+        store.shield.applications = selection.applicationTokens.isEmpty ?
+            nil : selection.applicationTokens
+        
+        // 카테고리 차단 설정
+        store.shield.applicationCategories = selection.categoryTokens.isEmpty
+        ? nil
+        : .specific(selection.categoryTokens)
+        
+        // 웹 도메인 차단 설정 (필요한 경우)
+        store.shield.webDomains = selection.webDomainTokens.isEmpty
+        ? nil
+        : selection.webDomainTokens
+    }
+    
+    func unblockApps() {
+        store.shield.applications = nil
+        store.shield.applicationCategories = nil
+        store.shield.webDomains = nil
     }
 }
